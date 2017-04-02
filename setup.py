6e01fb54714445ba709a1794e4ffecee1ae66928
Mirai Kim <nachtbeere@outlook.com>
2017-04-03 02:40:34 +0900

    modify setup

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 from hannakageul import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='hannakageul',
     version=__version__,
     description='Hangeul - Kana encoding converter',
+    long_description=long_description,
     author='Mirai Kim',
     author_email='nachtbeere@outlook.com',
+    license='MIT',
     url='https://github.com/noxowl/hannakageul',
-    packages=['hannakageul', 'hannakageul.convert'])
+    keywords='charset encoding converter',
+    packages=['hannakageul'])
